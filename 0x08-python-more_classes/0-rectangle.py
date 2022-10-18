5255cdd12e6594b3d7c9edcc6c96c876516f1bb2
Kalisa Jacques <kaljama63@gmail.com>
2022-10-18 18:45:10 +0200

    0-rectangle.py file

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Define a Rectangle class."""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Representation of Rectangle."""
+    """Represent a rectangle."""
     pass
