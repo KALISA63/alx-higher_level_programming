01473d7309f7a06c4865543d3253362e7899e1be
KALISA63 <kaljama63@gmail.com>
2022-12-21 15:30:48 +0200

    relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Provides a State class to map to objects in a states table
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Defines a State class to map to objects in a states table
+    """
+    __tablename__ = 'states'
+    id = Column(
+        Integer,
+        autoincrement=True,
+        nullable=False,
+        primary_key=True,
+        unique=True,
+    )
+    name = Column(
+        String(128),
+        nullable=False,
+    )
+    cities = relationship('City', cascade='all, delete-orphan')
