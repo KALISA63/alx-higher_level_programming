5d6605477bee1b40ea0a31fe3903870fbc668ad5
KALISA63 <kaljama63@gmail.com>
2022-12-21 15:09:19 +0200

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+List all states matching given name from a MySQL db on localhost at port 3306
+"""
+
+from mysqlman import MySQLMan
+from MySQLdb import Error
+from sys import argv, exit, stderr
+
+
+HELP = '{} username password database search'.format(argv[0])
+HOST = 'localhost'
+PORT = 3306
+
+
+if __name__ == '__main__':
+    try:
+        params = {
+            'user': argv[1],
+            'password': argv[2],
+            'database': argv[3],
+            'host': HOST,
+            'port': PORT,
+        }
+        search = argv[4]
+    except IndexError:
+        stderr.write('usage: {}\n'.format(HELP))
+        exit(2)
+    try:
+        mysqlman = MySQLMan(connect=True, **params)
+    except Error as e:
+        stderr.write('{}\n'.format(e.args[1]))
+        exit(1)
+    query = """
+    SELECT id, name
+    FROM states
+    WHERE BINARY name = '{}'
+    ORDER BY id;
+    """.format(search)
+    results = mysqlman.query([query, ()])
+    for row in results[0]:
+        print(row)
