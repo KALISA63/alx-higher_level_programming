9191377c3d733a83ef3bf46d7b828f49195eea38
KALISA63 <kaljama63@gmail.com>
2022-12-21 15:12:16 +0200

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+List all states matching given name from a MySQL db on localhost at port 3306
+"""
+
+from mysqlman import MySQLMan
+from MySQLdb import Error
+from sys import argv, exit, stderr
+
+
+HELP = '{} username password database search'.format(argv[0])
+HOST = 'localhost'
+PORT = 3306
+
+
+if __name__ == '__main__':
+    try:
+        params = {
+            'user': argv[1],
+            'password': argv[2],
+            'database': argv[3],
+            'host': HOST,
+            'port': PORT,
+        }
+        search = argv[4]
+    except IndexError:
+        stderr.write('usage: {}\n'.format(HELP))
+        exit(2)
+    try:
+        mysqlman = MySQLMan(connect=True, **params)
+    except Error as e:
+        stderr.write('{}\n'.format(e.args[1]))
+        exit(1)
+    query = """
+    SELECT c.name
+    FROM cities c, states s
+    WHERE c.state_id = s.id
+    AND s.name = %s
+    ORDER BY c.id;
+    """
+    results = mysqlman.query([query, (search,)])
+    print(', '.join(row[0] for row in results[0]))
